c0c4fdea045bc455dda8e3f35275b090648f4bb4
sudarshansoni2000 <sudarshansoni2017@gmail.com>
2024-05-17 23:14:55 +0530

    PreparedVector & Tested it using manual upload

--- src/utils/upload_file.py
+++ src/utils/upload_file.py
@@ -1,1 +1,44 @@
-from utils.prepare_vectored
+from utils.prepare_vectordb import PrepareVectorDB
+from typing import List , Tuple 
+from utils.load_config import LoadConfig
+
+APPCFG = LoadConfig()
+
+
+class UploadFile():
+
+    """
+    Utility class for handling file uploads and processing 
+    This class provides static methods for checking directories and processing upload files to prepare vectordb """
+
+    @staticmethod
+    def process_uploaded_files(files_dir : List , chatbot : List , rag_with_dropdown: str) -> Tuple:
+        """
+        Process uploaded files to prepare a VectorDB.
+
+        Parameters:
+            files_dir (List): List of paths to the uploaded files.
+            chatbot: An instance of the chatbot for communication.
+
+        Returns:
+            Tuple: A tuple containing an empty string and the updated chatbot instance.
+        """
+
+        if rag_with_dropdown == "Upload doc : Process for RAG":
+
+            prepare_vectordb_instance = PrepareVectorDB(data_directory=files_dir,
+                                                        persist_directory=APPCFG.custom_persist_directory,
+                                                        embedding_model_engine=APPCFG.embedding_model_engine,
+                                                        chunk_size=APPCFG.chunk_size,
+                                                        chunk_overlap= APPCFG.chunk_overlap)
+            
+            chatbot.append(
+                ("" , "Uploaded files are ready . Please ask your question")
+            )
+        # elif:
+        #     pass
+        
+        else: 
+            chatbot.append(
+                (" ", "If you would like to upload a PDF, please select your desired action in 'rag_with' dropdown."))
+        return "", chatbot

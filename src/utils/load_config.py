df98a7bf4fcd6198e4a537115bd469b5984a0eb9
sudarshansoni2000 <sudarshansoni2017@gmail.com>
2024-05-17 23:56:39 +0530

    PrepareVectorDb & tested manually

--- src/utils/load_config.py
+++ src/utils/load_config.py
@@ -119,7 +119,7 @@ class LoadConfig:
         # openai.api_type = os.getenv("OPENAI_API_TYPE")
         # openai.api_base = os.getenv("OPENAI_API_BASE")
         # openai.api_version = os.getenv("OPENAI_API_VERSION")
-        openai.api_key = "sk-proj-IVXbI5hXcFy2jEYtFCtnT3BlbkFJyz1yE5aKsYq7uTBLwXgM" #"sk-proj-a7JM7n1jmXuOah8BwrgTT3BlbkFJhbBBrauTbf9cglyOQFfx"
+        # openai.api_key = os.getenv("OPENAI_API_KEY") 
 
     def create_directory(self,directory_path :str):
         """
